65b0d1dbbf8ee7acdaae35a80f4ed83ec8c421a3
kylienorwood <norwoodk@moravian.edu>
2019-06-05 10:23:08 -0400

    Adjusted to model off given example.

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,9 +1,9 @@
 from Practice.app import app
 
 
-def test_get_page():
+def test_get_hello():
     app.config['TESTING'] = True
     client = app.test_client()
 
-    result = client.get('/')
+    result = client.get('/hello')
     assert b'Hello world!' in result.data
